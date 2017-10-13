27adfe53690efb96769a2f940f76d8f2c37d0c3c
derams <1281666753@qq.com>
2017-10-13 07:06:14 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style>
+  #app{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    background-color: teal;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <div class="login">
+      你是
+      <div class="input-wrap">
+        <input type="text" v-modal="massage" placeholder="你的名字">
+      </div>
+    </div>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    display: flex;
+    justify-content: center;
+    width: 60%;
+    background-color: white;
+    line-height: 50px;
+  }
+  .login{
+    display: flex;
+    justify-content: center;
+    background-color: white;
+    line-height: 50px;
+  }
+  .input-wrap{
+    margin-left: 20px;
+  }
+  input{
+    border:0;
+    border-bottom: 2px solid cyan;
+    outline: 0;
+
+  }
+</style>
